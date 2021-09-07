2cdd04de2c0c5a7024ae95f8f19f95adf0d65791
Jennifer Ferreira <jennifersouzaferreira@outlook.com>
2021-09-06 23:56:53 -0300

    Mostrando Lista de tarefas

--- app/src/main/java/com/example/listadetarefas/ui/AddTaskActivity.kt
+++ app/src/main/java/com/example/listadetarefas/ui/AddTaskActivity.kt
@@ -1,8 +1,8 @@
 package com.example.listadetarefas.ui
 
+import android.app.Activity
 import android.os.Bundle
 import android.util.Log
-import android.util.TimeFormatException
 import androidx.appcompat.app.AppCompatActivity
 import com.example.listadetarefas.databinding.ActivityAddTaskBinding
 import com.example.listadetarefas.datasource.TaskDataSource
@@ -14,11 +14,8 @@ import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
 import java.util.*
 
-
 class AddTaskActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityAddTaskBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,11 +23,10 @@ class AddTaskActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         insertListeners()
-
     }
     private fun insertListeners() {
         binding.date.editText?.setOnClickListener {
-           val datePicker = MaterialDatePicker.Builder.datePicker().build()
+            val datePicker = MaterialDatePicker.Builder.datePicker().build()
             datePicker.addOnPositiveButtonClickListener {
 
                 val timeZone = TimeZone.getDefault()
@@ -43,11 +39,14 @@ class AddTaskActivity : AppCompatActivity() {
 
         binding.hour.editText?.setOnClickListener {
             val timePicker = MaterialTimePicker.Builder()
-            .setTimeFormat(TimeFormat.CLOCK_24H).build()
+                .setTimeFormat(TimeFormat.CLOCK_24H).build()
 
             timePicker.addOnPositiveButtonClickListener {
-                binding.hour.text = "${timePicker.hour}:${timePicker.minute}"
+                val minute = if (timePicker.minute in 0..9) "0${timePicker.minute}" else timePicker.minute
+                val hour = if (timePicker.hour in 0..9) "0${timePicker.hour}" else timePicker.hour
+                binding.hour.text = "$hour:$minute"
             }
+
             timePicker.show(supportFragmentManager, null)
         }
         binding.cancelButton.setOnClickListener {
@@ -58,10 +57,14 @@ class AddTaskActivity : AppCompatActivity() {
             val task = Task(
                 title = binding.title.text,
                 date = binding.date.text,
-                hour = binding.hour.text,
+                hour = binding.hour.text
             )
             TaskDataSource.insertTask(task)
-            Log.e("TAG", "insertListeners: " + TaskDataSource.getList() )
+
+            setResult(Activity.RESULT_OK)
+            finish()
+
+
         }
     }
 
